1fedaeb6ec7a9033ed55f8ce4b41008dd48a2889
Руслан <ruslan.mirets@gmail.com>
2021-02-19 11:57:03 +0300

    Обновил модальное окно

--- js/main.js
+++ js/main.js
@@ -52,5 +52,27 @@ $(function () {
             errorElementBottom.innerText = messagesBottom.join('! ')
         }
     })
+
+    const phoneModal = document.getElementById('phone-modal')
+    const checkboxModal = document.getElementById('checkbox-modal')
+    const formModal = document.getElementById('modal-form')
+    const errorElementModal = document.getElementById('error-modal')
+
+    formModal.addEventListener('submit', (e) => {
+        let messagesModal = []
+        if (phoneModal.value === '' || phoneModal.value == null || phoneModal.value.length < 16) {
+            messagesModal.push('Введите номер телефона!')
+        }
+        else if (!checkboxModal.checked) {
+            messagesModal.push('Подтвердите согласие!')
+        }
+        else if (checkboxModal.checked) {
+            messagesModal.push('Спасибо, наш менеджер свяжется с Вами в ближайшее время.')
+        }
+        if (messagesModal.length > 0) {
+            e.preventDefault()
+            errorElementModal.innerText = messagesModal.join('! ')
+        }
+    })
     
 });
